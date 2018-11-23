1399fcb3b7a339718dac862f8ec7a81f18927d96
Bhavan Turaka <bhavanturaka21@gmail.com>
2018-11-24 02:03:32 +0530

    final model with 700 episodes

---
+++ final/nn2.py
@@ -0,0 +1,53 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+import numpy
+
+
+def defineModel():
+	model = Sequential()
+	model.add(Dense(100, input_dim=192, activation='relu'))
+	model.add(Dense(50, activation='relu'))
+	model.add(Dense(15, activation='relu'))
+	model.add(Dense(4, activation='linear'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+	return model
+
+
+def loadModel():
+	try:
+		json_file = open('model2.json', 'r')
+	except:
+		print("Creating JSON file 2 and weights file")
+		return defineModel()
+
+	model_json = json_file.read()
+	json_file.close()
+	model = model_from_json(model_json)
+	model.load_weights("model2.h5")
+
+	model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+
+	return model
+
+def saveModel(model):
+	model_json = model.to_json()
+	with open("model2.json", "w") as json_file:
+	    json_file.write(model_json)
+	model.save_weights("model2.h5")
+	# print("Saved model to disk")
+
+def getQ(model,X):
+	return model.predict(numpy.array([X]))[0]
+
+def train(model,X,Y):
+	model.fit(numpy.array(X), numpy.array(Y), epochs=1, batch_size=1000,verbose=0)
+
+
+
+# model = loadModel();
+# x  = numpy.array(numpy.array([[0]*768,[1]*768]))
+# y = model.predict(x)
+# print(y)
+# print(y[0][0])
